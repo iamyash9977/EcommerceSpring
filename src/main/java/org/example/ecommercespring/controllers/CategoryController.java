2ab6708093d46fe4ba385accb801319852fa3e6d
Yash Jagtap <jagtapyash9977@gmail.com>
2025-07-08 19:02:58 +0530

    Added @Qualifier for rest template

--- src/main/java/org/example/ecommercespring/controllers/CategoryController.java
+++ src/main/java/org/example/ecommercespring/controllers/CategoryController.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.core.format.InputAccessor;
 import org.example.ecommercespring.dto.CategoryDTO;
 import org.example.ecommercespring.services.ICategoryService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -28,6 +26,14 @@ public class CategoryController {
         List<CategoryDTO> result = this.categoryService.getAllCategories();
         return ResponseEntity.ok(result);
     }
+
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO created = categoryService.createCategory(categoryDTO);
+        return ResponseEntity.ok(created);
+    }
+
 
 
 
